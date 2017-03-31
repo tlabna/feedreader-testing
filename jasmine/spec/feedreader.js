f9e84eea038091f12f2d26985b796073bc945502
tlabna <tlabna@users.noreply.github.com>
2017-03-31 18:11:50 -0400

    Add test suit for menu with tests. All tested and passed.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -59,18 +59,30 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function() {
+        var menuIcon = document.querySelector(".menu-icon-link");
 
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('has menu element hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+        });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        /* TODO: Write a test that ensures the menu changes
+        * visibility when the menu icon is clicked. This test
+        * should have two expectations: does the menu display when
+        * clicked and does it hide when clicked again.
+        */
+        it('changes visiblity when the menu icon is clicked', function() {
+            menuIcon.click();
+            expect($('body').attr('class')).toBe('');
+            menuIcon.click();
+            expect($('body').attr('class')).toBe('menu-hidden');
+        });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
